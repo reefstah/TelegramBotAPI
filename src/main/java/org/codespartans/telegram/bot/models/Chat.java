f07918f86805ce50082682dc51a608279259050c
ralph <ralph.broers@luminis.eu>
2015-08-27 21:21:09 +0200

    See if travis get gradle with environment variables

--- src/main/java/org/codespartans/telegram/bot/models/Chat.java
+++ src/main/java/org/codespartans/telegram/bot/models/Chat.java
@@ -2,7 +2,7 @@ package org.codespartans.telegram.bot.models;
 
 /**
  * This object can either be a {@link org.codespartans.telegram.bot.models.User} or a {@link org.codespartans.telegram.bot.models.GroupChat}.
- * @See org.codespartans.telegram.bot.models.User  org.codespartans.telegram.bot.models.GroupChat
+ * @see org.codespartans.telegram.bot.models.User  org.codespartans.telegram.bot.models.GroupChat
  */
 public interface Chat {
     int getId();
--- src/main/java/org/codespartans/telegram/bot/models/Reply.java
+++ src/main/java/org/codespartans/telegram/bot/models/Reply.java
@@ -4,7 +4,7 @@ package org.codespartans.telegram.bot.models;
  * Additional interface options. A JSON-serialized object for a custom reply keyboard,
  * instructions to hide keyboard or to force a reply from the user.
  *
- * @See ReplyKeyboardMarkup ReplyKeyboardHide ForceReply
+ * @see ReplyKeyboardMarkup ReplyKeyboardHide ForceReply
  */
 public interface Reply {
 }
